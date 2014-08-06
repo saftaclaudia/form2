eb8c68891c7885acc15639cdf3b16214980ff929
saftaclaudia <saftaclaudia27@yahoo.com>
2014-08-06 16:39:14 +0300

    Revert "css right form"
    This reverts commit 75f8635b5150ebd2a51faff388daed9428fb571b.

--- form.php
+++ form.php
@@ -21,7 +21,7 @@
 					</select>
 				</div>
 				<div class="clearfix">
-					<label for="namensvor" class="paddingtop3">Namensvor-/zusatz</label>
+					<label for="namensvor">Namensvor-/zusatz</label>
 					<select class="width3" id="destination">
 						<option value="zu">zu</option>
 						<option value="on">on</option>
@@ -65,7 +65,7 @@
 						<option value="single">single</option>
 					</select>
 				</div >
-				<div class="clearfix inline"> 
+				<div class="clearfix" > 
 					<label class="confirm">Adress hat sich in den letzten e Jahren geander</label>
 					<input type="checkbox" id="confirm" class="width5">
 				</div>
@@ -87,7 +87,7 @@
 			<form id="right-form" action="processApplication.php" method="post">
 				<p class="background">Beruf</p>
 				<div class="clearfix">
-					<label for="art" class="paddingtop3">Art der Beschaftigung</label>
+					<label for="art">Art der Beschaftigung</label>
 					<select id="art">
 						<option value="Rentner(in)/Pensionar(in)">Rentner(in)/Pensionar(in)</option>
 						<option value="bllllllllll">Blllllllll</option>
@@ -98,22 +98,22 @@
 					<input id="beruf" type="text">
 				</div>
 				<div class="clearfix">
-					<label for="seit" class="margin">ungekundigt beschaftigt seit</label>
-					<input id="seit" type="date" min="01-01-2001" max="12-31-2001" class="width4">
+					<label for="seit">ungekundigt beschaftigt seit</label>
+					<input id="seit" type="date" min="01-01-2001" max="12-31-2001">
 				</div>
 				<div class="clearfix">
-					<label for="monatl" class="margin">Monatl. Nettoeinkommen</label>
-					<input id="monalt" type="text" class="width6">
+					<label for="monatl">Monatl.Nettoeinkommen</label>
+					<input id="monalt" type="text">
 				</div>
-				<p class="background margintop">Kontakt/Einverstandnis & Werbung</p>
+				<p class="background">Kontakt/Einverstandnis & Werbung</p>
 				<div class="clearfix">
 					<label for="teltag">Tel.(tagsuber)</label>
 					<select id="teltag">
 						<option value="DE">DE</option>
 						<option value="ME">ME</option>
 					</select>
-					<input type="text" id="teltag1" class="width1">
-					<input type="text" id="teltag2" class="width4">
+					<input type="text" id="teltag1">
+					<input type="text" id="teltag2">
 				</div>
 				<div class="clearfix">
 					<label for="telber">Tel.(Beratung)</label>
@@ -121,19 +121,19 @@
 						<option value="DE">DE</option>
 						<option value="ME">ME</option>
 					</select>
-					<input type="text" id="telber1" class="width1">
-					<input type="text" id="telber2" class="width4">
+					<input type="text" id="telber1">
+					<input type="text" id="telber2">
 				</div>
 				<div class="clearfix">
-					<label for="confirmtel" >Einverstandnis Telefonwerbung</label>
-					<input type="checkbox" id="confirmtel" class="position">
+					<label for="confirmtel">Einverstandnis Telefonwerbung</label>
+					<input type="checkbox" id="confirmtel">
 				</div>
-				<div class="clearfix margintop">
+				<div class="clearfix">
 					<label for="confirmjeder">jederzeit</label>
-					<input type="checkbox" id="confirmjeder" class="position">
+					<input type="checkbox" id="confirmjeder">
 				</div>
 				<div class="clearfix">
-					<label for="tagen" class="width25 margin">An folgenden tagen/zu folgenden Zeiten</label>
+					<label for="tagen">An folgenden tagen/zu folgenden Zeiten</label>
 					<input type="text" id="tagen" placeholder="Montag Vormittag oder Freitag al">
 				</div>
 				<div class="clearfix">
@@ -141,13 +141,15 @@
 					<input type="text" id="email-address" placeholder="max.mustermann@provider.de">
 				</div>
 				<div class="clearfix">
-					<label for="confirmemail" class="margin">Einverstandnis Emailwebung</label>
-					<input type="checkbox" id="confirmemail" class="position">
+					<label for="confirmemail">Einverstandnis Emailwebung</label>
+					<input type="checkbox" id="confirmemail">
 				</div>
-				<div class="clearfix inline">
-					<label for="cornfirm2" class="confirm width100">Der Kunde wiligt in die Ubermittlung der in der SCHUFA-Erklarung genannten Daten an die SCHUFA-Hoding AG ein.</label>
-					<label for="yes" class="width1"><input id="yes" type="radio" name="confirm" class="position"> ja</label>
-					<label for="no" class="width1"><input id="no" type="radio" name="confirm" class="position">nein</label>	
+				<div class="clearfix">
+					<label for="cornfirm2">Der Kunde wiligt in die Ubermittlung der in der SCHUFA-Erklarung genannten Daten an die SCHUFA-Hoding AG ein.</label>
+					<label for="yes">ja</label>
+					<input id="yes" type="radio" name="confirm">
+					<label for="no">nein</label>
+					<input id="no" type="radio" name="confirm">
 				</div>
 			</form>
 		</div>
